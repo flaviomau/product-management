c3e2bd79a1568471f355ac7bc321f3a3fa0a1e15
flaviomau <flaviomau@yahoo.com>
2018-06-28 17:30:38 -0300

    created db json and async request to retrieve data for categories

--- src/Products.js
+++ src/Products.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
+import axios from 'axios'
 
 import ProductsHome from './ProductsHome'
 import Category from './Category'
 
-class Products extends Component{    
+class Products extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            categories: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3001/categories')
+            .then(res => {
+                this.setState({
+                    categories: res.data
+                })
+            })
+    }
+
+    renderCategory(cat){
+        return (
+            <li key={cat}>
+                <Link to={`/products/category/${cat.id}`}>{cat.category}</Link>
+            </li>
+        )
+    }
+
     render(){
         const { match } = this.props
+        const { categories } = this.state
         return (
             <div className='row'>
                 <div className='col-md-2'>
                     <h3>Category</h3>
-                    <Link to='products/category/1'>Category 1</Link>
+                    <ul>
+                        {categories.map(this.renderCategory)}
+                    </ul>
                 </div>
                 <div className='col-md-10'>                    
                     <h1>Products</h1>
